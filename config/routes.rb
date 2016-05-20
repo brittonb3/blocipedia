bed62e31348a2c2d41fa5cfeb057235980c73925
Britton Baumann <brittonbaumann@gmail.com>
2016-05-20 09:12:51 -0500

    CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  resources :wikis
+
   devise_for :users
   get 'welcome/index'
 
   get 'welcome/about'
 
-  root 'welcome#index'
+  root 'wikis#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

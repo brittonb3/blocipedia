6103ab3ddbb219f4769e68517087306c383867d2
Britton Baumann <brittonbaumann@gmail.com>
2016-05-22 14:07:20 -0500

    Seeding data, Users and wikis

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+50.times do
+ Wiki.create!(
+  title: Faker::Lorem.characters(10),
+  body: Faker::Lorem.sentences(3)
+ )
+ end
+ wikis = Wiki.all
+
+ 10.times do
+   User.create!(
+   email: Faker::Internet.email,
+   password: Faker::Internet.password(8),
+   role: 'standard'
+   )
+ end
+ users = User.all
+
+
+ puts "Seed finished"
+ puts "#{Wiki.count} wikis created"
+ puts "#{User.count} users created"
